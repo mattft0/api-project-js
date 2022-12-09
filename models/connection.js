26f23b7743da1d09687335198a0ecf142fa60214
mattft0 <94959885+mattft0@users.noreply.github.com>
2022-12-09 14:31:20 +0000

    changement de bdd

--- models/connection.js
+++ models/connection.js
@@ -1,12 +1,8 @@
-const Sequelize = require("sequelize");
-const dbConfig = require("./db-config");
+const Sequelize = require('sequelize');
 
-const connection = new Sequelize(dbConfig.DATABASE, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  dialect: dbConfig.DIALECT,
-  port: '3000'
+const connection = new Sequelize('carshop', 'root', 'root', {
+  host: 'localhost',
+  dialect: 'mariadb',
 });
-
-connection.authenticate().then(() => console.log("Connected to DB"));
 
 module.exports = connection;
--- models/db-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    HOST: 'localhost',
-    USER: 'root',
-    PASSWORD: 'root',
-    DATABASE: 'carshop',
-    DIALECT: 'mysql',
-};
